334fcc76fd70384cea9d92323aa07f9d9850a024
SergiVera <sergitus1998@gmail.com>
2018-12-20 08:45:16 +0100

    implement login activity

--- app/src/main/java/dsa/eetac/upc/edu/APIRest.java
+++ app/src/main/java/dsa/eetac/upc/edu/APIRest.java
@@ -3,12 +3,18 @@ package dsa.eetac.upc.edu;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public interface APIRest {
     //We specify the url
-    String BASE_URL = "";
+    String BASE_URL = "https://do.diba.cat/api/dataset/municipis/format/json/pag-ini/1/pag-fi/11";
+
+    //We get the Data from the API
+    @GET()
+    Call<Element> getData();
 
     static APIRest createAPIRest() {
         Gson gson = new GsonBuilder()
--- app/src/main/java/dsa/eetac/upc/edu/MainActivity.java
+++ app/src/main/java/dsa/eetac/upc/edu/MainActivity.java
@@ -1,13 +1,50 @@
 package dsa.eetac.upc.edu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
+
+    private APIRest myapirest;
+    private Recycler recycler;
+    private RecyclerView recyclerView;
+
+    TextView textViewIne;
+    TextView textViewNameTown;
+    ImageView ivImageFromUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recycler = new Recycler(this);
+        recyclerView.setAdapter(recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //TextViews where we show the Ine, the name of the local council and the image of the local council
+        textViewIne = findViewById(R.id.numeroIne);
+        textViewNameTown = findViewById(R.id.nomMunicipi);
+        ivImageFromUrl = findViewById(R.id.escutMunicipi);
+
+        // Get the Intent that started this activity
+        Intent intent = getIntent();
+
+        myapirest = APIRest.createAPIRest();
+
+        getIne();
+    }
+
+    private void getIne() {
+        Call<Element> elementCall = myapirest.getIne();
     }
 }
